ee26e332c5d2f0b369758becc8b5254268a9a285
Tanner Hemphill <tannerkhemphill@gmail.com>
2021-12-11 20:32:11 -0500

    Add book and library classes and move associated functionality into them

--- script.js
+++ script.js
@@ -1,94 +1,98 @@
-let myLibrary = [];
+class Book {
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${status}`;
-    };
-};
-
-Book.prototype.toggleRead = function() {
-    if (this.status === "Have read") {
-        this.status = "Not read";
+    constructor(title, author, pages, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
     }
-    else {
-        this.status = "Have read";
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`;
     }
-    displayBooks();
-};
 
-function addBookToLibrary(title, author, pages, status) {
-    let book = new Book(title, author, pages, status);
-    myLibrary.push(book);
-    displayBooks();
-};
-
-const library = document.querySelector('#container');
-
-function displayBooks() {
-    while (library.firstChild) {
-        library.removeChild(library.firstChild);
+    toggleRead() {
+        if (this.status === "Have read") {
+            this.status = "Not read";
+        }
+        else {
+            this.status = "Have read";
+        }
     }
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.dataset.index = i;
-        card.textContent = `Title: ${book.title} \r\nAuthor: ${book.author} \r\nPages: ${book.pages} \r\nStatus: ${book.status}`;
-        library.appendChild(card);
+}
+
+class Library {
+
+    constructor(library = []) {
+        this.library = library;
     }
-    addButtons();
-    let removes = document.querySelectorAll('.remove');
-    removes.forEach((remove) => {
-        remove.addEventListener('click', (event) => {
-            let target = event.currentTarget;
-            let parent = target.parentNode;
-            let div = parent.parentNode;
-            let index = parseInt(div.dataset.index);
-            removeBook(index);
+
+    addBookToLibrary(title, author, pages, status) {
+        let book = new Book(title, author, pages, status);
+        this.library.push(book);
+        this.displayBooks();
+    }
+
+    removeBook(index) {
+        this.library.splice(index, 1);
+        this.displayBooks();
+    }
+
+    displayBooks() {
+        const library = document.querySelector('#container');
+        while (library.firstChild) {
+            library.removeChild(library.firstChild);
+        }
+        for (let i = 0; i < this.library.length; i++) {
+            let book = this.library[i];
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.dataset.index = i;
+            card.textContent = `Title: ${book.title} \r\nAuthor: ${book.author} \r\nPages: ${book.pages} \r\nStatus: ${book.status}`;
+            library.appendChild(card);
+        }
+        let cards = document.querySelectorAll('.card');
+        cards.forEach((card) => {
+            let container = document.createElement('div');
+            container.classList.add('button-container');
+            card.appendChild(container);
+            let button1 = document.createElement('button');
+            let button2 = document.createElement('button');
+            button1.classList.add('remove');
+            button2.classList.add('toggle');
+            button1.innerHTML = 'X';
+            button2.innerHTML = 'Read';
+            container.appendChild(button1);
+            container.appendChild(button2);
         })
-    });
-    let toggles = document.querySelectorAll('.toggle');
-    toggles.forEach((toggle) => {
-        toggle.addEventListener('click', (event) => {
-            let target = event.currentTarget;
-            let parent = target.parentNode;
-            let div = parent.parentNode;
-            let index = parseInt(div.dataset.index);
-            myLibrary[index].toggleRead();
+        let removes = document.querySelectorAll('.remove');
+        removes.forEach((remove) => {
+            remove.addEventListener('click', (event) => {
+                let target = event.currentTarget;
+                let parent = target.parentNode;
+                let div = parent.parentNode;
+                let index = parseInt(div.dataset.index);
+                this.removeBook(index);
+            })
         })
-    });
+        let toggles = document.querySelectorAll('.toggle');
+        toggles.forEach((toggle) => {
+            toggle.addEventListener('click', (event) => {
+                let target = event.currentTarget;
+                let parent = target.parentNode;
+                let div = parent.parentNode;
+                let index = parseInt(div.dataset.index);
+                this.library[index].toggleRead();
+                this.displayBooks();
+            })
+        })
+    }
 };
 
-function addButtons() {
-    let cards = document.querySelectorAll('.card');
-    cards.forEach((card) => {
-        let container = document.createElement('div');
-        container.classList.add('button-container');
-        card.appendChild(container);
-        let button1 = document.createElement('button');
-        let button2 = document.createElement('button');
-        button1.classList.add('remove');
-        button2.classList.add('toggle');
-        button1.innerHTML = 'X';
-        button2.innerHTML = 'Read';
-        container.appendChild(button1);
-        container.appendChild(button2);
-    })
-};
-
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks();
-};
-
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', 'Have read')
-addBookToLibrary('Dune', 'Frank Herbert', '450', 'Not read');
-addBookToLibrary('The Count of Monte Cristo', 'Alexandre Dumas', '900', 'Have read');
-displayBooks();
+let myLibrary = new Library();
+myLibrary.addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', 'Have read')
+myLibrary.addBookToLibrary('Dune', 'Frank Herbert', '450', 'Not read');
+myLibrary.addBookToLibrary('The Count of Monte Cristo', 'Alexandre Dumas', '900', 'Have read');
+myLibrary.displayBooks();
 
 const toggleModal = () => {
     document.querySelector('.modal').classList.toggle('modal--hidden');
@@ -109,7 +113,7 @@ function retrieveFormInput() {
     let pages = form.elements['pages'].value;
     let status = form.elements['status'].value;
     if (title !== "" && author != "" && pages !== "" && status != "") {
-        addBookToLibrary(title, author, pages, status);
+        myLibrary.addBookToLibrary(title, author, pages, status);
     }
 }
 
